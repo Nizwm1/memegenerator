c21718f7885d0a891a9b302273a7183eb09e84da
Nizwm Borgoyari <borgoyarinizwm@gmail.com>
2022-05-09 20:22:55 +0530

    Second commit

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,13 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header>
-      <img />
-      <h2>Meme Generator</h2>
+    <header className="header">
+      <img
+        className="header--image"
+        src={process.env.PUBLIC_URL + "/images/troll-face.png"}
+      />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
     </header>
   );
 }
